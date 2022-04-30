1720033a91523d3c80165d2cfea3e1d5bde359aa
Kinnitchi <igorivieira97@gmail.com>
2022-04-30 12:04:00 -0300

    added the bird the animate for bird

--- src/js/script.js
+++ src/js/script.js
@@ -5,24 +5,24 @@ function newElement(tagName, className) {
 }
 
 function Barrel(reverse = false) {
-  this.divBarrels = newElement('div', 'barrel');
+  this.elementDom = newElement('div', 'barrel');
 
   const border = newElement('div', 'border');
   const body = newElement('div', 'body');
-  this.divBarrels.appendChild(reverse ? body : border);
-  this.divBarrels.appendChild(reverse ? border : body);
+  this.elementDom.appendChild(reverse ? body : border);
+  this.elementDom.appendChild(reverse ? border : body);
 
   this.setHeight = height => body.style.height = `${height}px`;
 }
 
 function BarrelsPair(height, aperture, x) {
-  this.divBarrels = newElement('div', 'barrel-pair');
+  this.elementDom = newElement('div', 'barrel-pair');
 
   this.roof = new Barrel(true);
   this.ground = new Barrel(false);
 
-  this.divBarrels.appendChild(this.roof.divBarrels);
-  this.divBarrels.appendChild(this.ground.divBarrels);
+  this.elementDom.appendChild(this.roof.elementDom);
+  this.elementDom.appendChild(this.ground.elementDom);
 
   this.sortAperture = () => {
     const heightRoof = Math.random() * (height - aperture);
@@ -31,9 +31,9 @@ function BarrelsPair(height, aperture, x) {
     this.ground.setHeight(heightGround);
   }
 
-  this.getX = () => parseInt(this.divBarrels.style.left.split('px')[0]);
-  this.setX = x => this.divBarrels.style.left = `${x}px`;
-  this.getLarge = () => this.divBarrels.clientWidth;
+  this.getX = () => parseInt(this.elementDom.style.left.split('px')[0]);
+  this.setX = x => this.elementDom.style.left = `${x}px`;
+  this.getLarge = () => this.elementDom.clientWidth;
 
   this.sortAperture();
   this.setX(x);
@@ -47,7 +47,7 @@ function Barrels(height, large, aperture, space, notifyPoint) {
     new BarrelsPair(height, aperture, large + space * 3)
   ];
 
-  const displacement = 3;
+  const displacement = 2;
   this.animate = () => {
     this.pair.forEach(pair => {
       pair.setX(pair.getX() - displacement);
@@ -66,11 +66,41 @@ function Barrels(height, large, aperture, space, notifyPoint) {
   }
 }
 
+function Bird(heightGame) {
+  let fly = false;
+
+  this.elementDom = newElement('img', 'bird');
+  this.elementDom.src = '../src/styles/imgs/bird.png';
+
+  this.getY = () => parseInt(this.elementDom.style.bottom.split('px')[0]);
+  this.setY = y => this.elementDom.style.bottom = `${y}px`;
+
+  window.onkeydown = e => fly = true;
+  window.onkeyup = e => fly = false;
+
+  this.animate = () => {
+    const newY = this.getY() + (fly ? 5 : -2);
+    const heightMax = heightGame - this.elementDom.clientHeight;
+
+    if (newY <= 0) {
+      this.setY(0)
+    } else if (newY >= heightMax) {
+      this.setY(heightMax)
+    } else {
+      this.setY(newY)
+    }
+  }
+  this.setY(heightGame / 2)
+}
 
 // TESTE ANIMAÇÂP
 const b = new Barrels(700, 1200, 200, 400);
+const bird = new Bird(700)
 const a = document.querySelector('[wm-flappy]');
-b.pair.forEach(pair => a.appendChild(pair.divBarrels));
+
+a.appendChild(bird.elementDom)
+b.pair.forEach(pair => a.appendChild(pair.elementDom));
 setInterval(() => {
-  b.animate()
+  b.animate();
+  bird.animate();
 }, 20);
